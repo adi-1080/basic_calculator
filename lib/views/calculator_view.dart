9363bea83f5d4ac2591530350507548cc801f3c6
adi-1080 <adityagupta5277@gmail.com>
2024-07-03 23:30:28 +0530

    Added about

--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -242,12 +242,12 @@ class _CalculatorViewState extends State<CalculatorView> {
   }
 }
 
-//Use extract method 
+//Use extract method
 class Display extends StatelessWidget {
   const Display({
     super.key,
-    this.hint = "Enter a number", 
-    required this.controller, 
+    this.hint = "Enter a number",
+    required this.controller,
     required this.focusNode,
     this.readOnly = true,
   });
@@ -314,7 +314,7 @@ Widget createNumButton(String text, FocusNode xFocusNode, FocusNode yFocusNode, 
         }
       }
     },
-    child: 
+    child:
     Text(
       text,
       style: TextStyle(
@@ -325,23 +325,3 @@ Widget createNumButton(String text, FocusNode xFocusNode, FocusNode yFocusNode, 
     ),
   );
 }
-
-//Flutter documentation code to hide keyboard whenever use taps on text field to input numbers
-// class HideKeyBoard extends StatefulWidget {
-//   const HideKeyBoard({super.key});
-
-//   @override
-//   State<HideKeyBoard> createState() => _HideKeyBoardState();
-// }
-
-// class _HideKeyBoardState extends State<HideKeyBoard> {
-//   final FocusNode myNode = FocusNode();
-//   @override
-//   Widget build(BuildContext context) {
-//     return GestureDetector(
-//       onTap: () {
-//         myNode.unfocused();
-//       },
-//     );
-//   }
-// }
---
+++ lib/views/hide.dart
@@ -0,0 +1,19 @@
+//Flutter documentation code to hide keyboard whenever use taps on text field to input numbers
+// class HideKeyBoard extends StatefulWidget {
+//   const HideKeyBoard({super.key});
+
+//   @override
+//   State<HideKeyBoard> createState() => _HideKeyBoardState();
+// }
+
+// class _HideKeyBoardState extends State<HideKeyBoard> {
+//   final FocusNode myNode = FocusNode();
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(
+//       onTap: () {
+//         myNode.unfocused();
+//       },
+//     );
+//   }
+// }
